7ecf75e6387907e28071de87a91bf1688462eb32
Wagner Jose da Silva <wagner.silva@lagoasoft.com>
2022-07-01 00:36:17 -0300

    help

--- packages/navigation/src/pages/Help.tsx
+++ packages/navigation/src/pages/Help.tsx
@@ -1,14 +1,16 @@
-import { Text } from "@monorepo/texts/src/Components";
+import { useText } from "@monorepo/texts";
 import { TEXT_KEYS } from "@monorepo/texts/src/keys";
 import { Paper, Stack } from "@mui/material";
 import React from "react";
-
+// @ts-ignore
+import HTMLRenderer from "react-html-renderer";
 export const Help: React.FC = () => {
+  const text = useText({ key: TEXT_KEYS.HELP_CONTENT });
   return (
-    <Stack width='80vw' p={3} spacing={2}>
+    <Stack width='80vw' p={0}>
       <Paper>
-        <Stack p={10}>
-          <Text messageKey={TEXT_KEYS.HELP_CONTENT} />
+        <Stack p={5}>
+          <HTMLRenderer html={text} />
         </Stack>
       </Paper>
     </Stack>
--- packages/texts/src/pt_br.ts
+++ packages/texts/src/pt_br.ts
@@ -1,3 +1,45 @@
+const HELP_CONTENT = `
+<p>
+A Árvore de Testes se apresenta como uma ferramenta interessante e eficaz ao ser adicionada ao fluxo de trabalho do controle de qualidade de uma empresa de desenvolvimento de software. Desenvolvida com tecnologias atuais, no entanto, consolidadas como React, NodeJS e MongoDB, otimizada para instalação em nuvem e com foco em instalações mais baratas ou até de graça, torna-se uma ferramenta com um baixíssimo impacto financeiro para seu uso. Com extrema capacidade e facilidade para melhorias na implementação e customizações, por ser de código-aberto pode ser incrementada de uma forma que toda a comunidade de usuários beneficiará das melhorias.
+</p>
+<div style="height: 30px"></div>
+<br />
+<h1><strong>Projetos</strong></h1>
+<div style="height: 10px"></div>
+<div>
+Os projetos dentro da aplicação servem para dividirmos aplicações de fato, desta forma poderemos ter fluxos de trabalho isolados e organizados.
+</div>
+<div style="height: 10px"></div>
+<h1><strong>Versões</strong></h1>
+<div style="height: 10px"></div>
+<div >
+As versões no que lhe concerne organizam os itens de um projeto internamente por lançamento, desta forma podemos visualizar os resultados por uma versão mais nova ou mais antiga rapidamente. Além de podermos vincular mais de uma versão a cada caso, execução ou roteiro de teste.
+</div>
+<div style="height: 10px"></div>
+<h1><strong>Funcionalidades</strong></h1>
+<div style="height: 10px"></div>
+<div>
+As funcionalidades são responsáveis de, a grosso modo, dividir os testes em sessões específicas, onde podemos, por exemplo, termos uma funcionalidade de autenticação, outra de busca de produto e assim por diante.
+</div>
+<div style="height: 10px"></div>
+<h1><strong>Casos de Teste</strong></h1>
+<div style="height: 10px"></div>
+<div>
+Os casos de teste tem papel principal no fluxo de trabalho, é nele que escreveremos os passos para validação em cada teste, usando o formato BDD (Behavior Driven Development).
+</div>
+<h1><strong>Execuções de Teste</strong></h1>
+<div style="height: 10px"></div>
+<div>
+Quando executarmos nossos casos de teste, é que nessa sessão que termos os resultados, organizados por data de execução e por executor.
+</div>
+<div style="height: 10px"></div>
+<h1><strong>Roteiros de Teste</strong></h1>
+<div style="height: 10px"></div>
+<div>
+Com os roteiros de teste poderemos unir mais de um caso de teste para ser executado em uma sequência, desta forma poderemos obter mais cobertura dos features nas execuções dos testes.
+</div>
+`;
+
 export default {
   TEST_TREE: "Arvore de Testes",
   USERNAME: "Usuário",
@@ -50,6 +92,5 @@ export default {
   RESULT: "Resultado",
   VALIDATION_STRING_REQUIRED: "Campo Obrigatório",
   VALIDATION_STRING_EMAIL: "Formato incorreto de e-mail",
-  HELP_CONTENT:
-    "A Árvore de Testes se apresenta como uma ferramenta interessante e eficaz ao ser adicionada ao fluxo de trabalho do controle de qualidade de uma empresa de desenvolvimento de software. Desenvolvida com tecnologias atuais, no entanto consolidadas como React, NodeJS e MongoDB, otimizada para instalação em nuvem e com foco em instalações mais baratas ou até de graça, torna-se uma ferramenta com um baixíssimo impacto financeiro para seu uso. Com extrema capacidade e facilidade para melhorias na implementação e customizações, por ser de código-aberto pode ser incrementada de uma forma que toda a comunidade de usuários beneficiará das melhorias.",
+  HELP_CONTENT,
 };
